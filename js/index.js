eb2c0d0c144a43fa05fafad0a919b4359b7325ae
Abdo8771 <ag9259710@gmail.com>
2024-07-20 03:22:56 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// function addElement() {
+//    var x=document.getElementById("element").value;
+
+//    document.getElementById("output").innerText+= x ;
+//    document.getElementById("output").innerText+= "\n" 
+
+
+
+
+// }
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json())
+.then(posts => posts.forEach(data => 
+{
+ var apiContent=document.getElementById("content");
+
+  var post=document.createElement('div');
+  post.classList.add('post', 'col-md-6' )
+
+
+
+  ;
+   post.innerHTML=`
+
+   <h2>${data.title}</h2>
+   <p>${data.body}</p>
+   `
+   ;
+   apiContent.appendChild(post);
+
+   
+   
+     
+    
+})
+);
+
